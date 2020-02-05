dccaf86816d052887582e2640919af09f9b7126e
Eliot Henson <eliotrhenson@gmail.com>
2020-02-05 19:26:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:langauges]
+programmer_hash[:dennis_ritchie][:langauges][0]
 end
 
 def adding_matz
